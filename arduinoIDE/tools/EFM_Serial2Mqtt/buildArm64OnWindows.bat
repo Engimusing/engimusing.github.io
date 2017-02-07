9832fd0b5a5feb97cfd2db4027869d515888b2c1
tdgeorge <github@tdgeorge.com>
2017-02-06 17:05:32 -0700

    Added linux x64-64 unique version to the json file, added arm64 built version of Serial2Mqtt

---
+++ arduinoIDE/tools/EFM_Serial2Mqtt/buildArm64OnWindows.bat
@@ -0,0 +1,21 @@
+rem script for cxfreezing a python script for osx from a windows machine. 
+rem This requires an ODROID C1 with ssh turned on and a shared fullder setup on the windows box at ODROID_SHARE
+rem It also requires bash for windows is installed. It could be rewritten to use another ssh client to remove this requirement if needed.
+rem Need version < 5.0 of cxfreeze on the ODROID. cxfreeze 5+ is broken on linux.
+set SCRIPT_FOLDER=/home/odroid/cxfreeze/EFM_Serial2Mqtt
+set SCRIPT_FILENAME=EFM_Serial2Mqtt.py
+
+set ODROID_SHARE=O:\cxfreeze
+
+mkdir %ODROID_SHARE%\EFM_Serial2Mqtt\
+copy /Y .\%SCRIPT_FILENAME% %ODROID_SHARE%\EFM_Serial2Mqtt\
+
+bash -c "ssh odroid@192.168.1.15 'cd %SCRIPT_FOLDER%; /usr/bin/cxfreeze ./%SCRIPT_FILENAME%  --target-dir=linuxArm64'"
+
+mkdir linuxArm64
+
+copy /Y %ODROID_SHARE%\EFM_Serial2Mqtt\linuxArm64\* linuxArm64\
+rmdir %ODROID_SHARE%\EFM_Serial2Mqtt /Q /S
+
+
+pause
--- arduinoIDE/tools/buildOsTars.sh
+++ arduinoIDE/tools/buildOsTars.sh
@@ -14,6 +14,18 @@ tar czvf efm_tools-1.0-linux32.tar.gz efm_tools-1.0
 rm -r efm_tools-1.0/efm_upload/*
 rm -r efm_tools-1.0/EFM_Serial2Mqtt/*
 
+#setup linux64-bit
+cp efm_upload/efm_upload.py efm_tools-1.0/efm_upload/
+cp EFM_Serial2Mqtt/EFM_Serial2Mqtt.py efm_tools-1.0/EFM_Serial2Mqtt/
+
+cp -r efm_upload/linux efm_tools-1.0/efm_upload/linux
+cp -r EFM_Serial2Mqtt/ubuntu efm_tools-1.0/EFM_Serial2Mqtt/linux
+
+tar czvf efm_tools-1.0-linux64.tar.gz efm_tools-1.0
+
+rm -r efm_tools-1.0/efm_upload/*
+rm -r efm_tools-1.0/EFM_Serial2Mqtt/*
+
 #setup windows 32-bit
 cp efm_upload/efm_upload.py efm_tools-1.0/efm_upload/
 cp EFM_Serial2Mqtt/EFM_Serial2Mqtt.py efm_tools-1.0/EFM_Serial2Mqtt/
@@ -45,14 +57,14 @@ rm *.tar
 echo \"systems\": > modules.json
 echo [ >> modules.json
 echo { >> modules.json
-echo \"url\": \"http://engimusing.github.io/arduinoIDE/tools/efm_tools-1.0-linux32.tar.gz\", >> modules.json
+echo \"url\": \"http://engimusing.github.io/arduinoIDE/tools/efm_tools-1.0-linux64.tar.gz\", >> modules.json
 echo -n \"checksum\": \"SHA-256: >> modules.json    
-sha256sum efm_tools-1.0-linux32.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
+sha256sum efm_tools-1.0-linux64.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
 echo \", >> modules.json
 echo \"host\": \"x86_64-pc-linux-gnu\", >> modules.json
-echo \"archiveFileName\": \"efm_tools-1.0-linux32.tar.gz\", >> modules.json
+echo \"archiveFileName\": \"efm_tools-1.0-linux64.tar.gz\", >> modules.json
 echo -n \"size\": \" >> modules.json
-wc -c efm_tools-1.0-linux32.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
+wc -c efm_tools-1.0-linux64.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
 echo \" >> modules.json
 echo }, >> modules.json
 
