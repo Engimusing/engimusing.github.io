4e9f9224e6ddf926d2ff17c127508b6a639b5624
tdgeorge <github@tdgeorge.com>
2017-02-07 15:36:40 -0700

    Adding new package for ARM32 bit.

--- arduinoIDE/tools/buildOsTars.sh
+++ arduinoIDE/tools/buildOsTars.sh
@@ -50,8 +50,22 @@ tar czvf efm_tools-1.0-osx64.tar.gz efm_tools-1.0
 rm -r efm_tools-1.0/efm_upload/*
 rm -r efm_tools-1.0/EFM_Serial2Mqtt/*
 
+#setup arm 32-bit
+cp efm_upload/efm_upload.py efm_tools-1.0/efm_upload/
+cp EFM_Serial2Mqtt/EFM_Serial2Mqtt.py efm_tools-1.0/EFM_Serial2Mqtt/
+
+cp -r efm_upload/linux efm_tools-1.0/efm_upload/linux
+cp -r EFM_Serial2Mqtt/linuxArm32 efm_tools-1.0/EFM_Serial2Mqtt/linux
+
+tar czvf efm_tools-1.0-linuxArm32.tar.gz efm_tools-1.0
+
+rm -r efm_tools-1.0/efm_upload/*
+rm -r efm_tools-1.0/EFM_Serial2Mqtt/*
+
 rm -r efm_tools-1.0/
 rm *.tar
+
+
 
 #x86_64-pc-linux-gnu
 echo \"systems\": > modules.json
@@ -80,6 +94,20 @@ echo -n \"size\": \" >> modules.json
 wc -c efm_tools-1.0-linux32.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
 echo \" >> modules.json
 echo }, >> modules.json
+
+#Arm32
+echo { >> modules.json
+echo \"url\": \"http://engimusing.github.io/arduinoIDE/tools/efm_tools-1.0-linuxArm32.tar.gz\", >> modules.json
+echo -n \"checksum\": \"SHA-256: >> modules.json    
+sha256sum efm_tools-1.0-linuxArm32.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
+echo \", >> modules.json
+echo \"host\": \"arm-linux-gnueabihf\", >> modules.json
+echo \"archiveFileName\": \"efm_tools-1.0-linuxArm32.tar.gz\", >> modules.json
+echo -n \"size\": \" >> modules.json
+wc -c efm_tools-1.0-linuxArm32.tar.gz | grep -Eo '^[^ ]+' | tr -d '\n' >> modules.json
+echo \" >> modules.json
+echo }, >> modules.json
+
 
 #i686-mingw32
 echo { >> modules.json
