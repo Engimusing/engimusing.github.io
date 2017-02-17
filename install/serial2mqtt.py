998328e678ec21b74578ac355e920b74850c2984
Joe George <joeg@engimusing.com>
2017-02-16 19:24:26 -0700

    set up for logging

--- install/serial2mqtt.py
+++ install/serial2mqtt.py
@@ -29,6 +29,48 @@ import paho.mqtt.client as mqtt
 import sys
 import time
 import getopt
+import logging
+import logging.handlers
+
+LOG_FILENAME = "/var/log/serial2mqtt/serial2mqtt.log"
+LOG_LEVEL = logging.info
+
+# Configure logging to log to a file, making a new file at midnight and keeping the last 3 day's data
+# Give the logger a unique name (good practice)
+logger = logging.getLogger(__name__)
+
+# Set the log level to LOG_LEVEL
+logger.setLevel(LOG_LEVEL)
+
+# Make a handler that writes to a file, making a new file at midnight and keeping 3 backups
+handler = logging.handlers.TimedRotatingFileHandler(LOG_FILENAME, when="midnight", backupCount=3)
+
+# Format each log message like this
+formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
+# Attach the formatter to the handler
+handler.setFormatter(formatter)
+
+# Attach the handler to the logger
+logger.addHandler(handler)
+
+# Make a class we can use to capture stdout and sterr in the log
+class MyLogger(object):
+        def __init__(self, logger, level):
+                """Needs a logger and a logger level."""
+                self.logger = logger
+                self.level = level
+
+        def write(self, message):
+                # Only log if there is a message (not just a new line)
+                if message.rstrip() != "":
+                        self.logger.log(self.level, message.rstrip())
+
+# Replace stdout with logging to file at INFO level
+sys.stdout = MyLogger(logger, logging.INFO)
+
+# Replace stderr with logging to file at ERROR level
+sys.stderr = MyLogger(logger, logging.ERROR)
 
 try:
     import fcntl
@@ -82,8 +124,8 @@ def getSerialPort():
 
 class toSerialThread(threading.Thread):
     """ A worker thread that receives strings from the toSerialPort_q queue
-        and sends them out the serial port.
-    """
+and sends them out the serial port.
+"""
     def __init__(self, toSerialPort_q, serialPort):
         super(toSerialThread, self).__init__()
         self.toSerialPort_q = toSerialPort_q
@@ -109,8 +151,8 @@ class toSerialThread(threading.Thread):
 
 class fromSerialThread(threading.Thread):
     """ A worker thread that receives strings from the serial port and
-        puts them in the fromSerialPort_q queue.
-    """
+puts them in the fromSerialPort_q queue.
+"""
     
     jsonStr = ""
 
@@ -188,12 +230,12 @@ def on_disconnectp(mqttp, userdata, rc):
 def on_log(mqttp, userdata, level, buf):
     if level == mqtt.MQTT_LOG_WARNING:
         print buf
-"""
+        """
 
 class toMQTT(threading.Thread):
     """ A worker thread that receives strings from the fromSerialPort_q queue
         and sends them to the MQTT client.
-    """
+        """
     def __init__(self, fromSerialPort_q):
         super(toMQTT, self).__init__()
         self.fromSerialPort_q = fromSerialPort_q
